e95df41a50b4aaf43f006e7927022b608a34be7e
Carlos Julian Mestanza Novoa <julian.cmn@gmail.com>
2020-04-18 18:36:49 -0500

    Update Idesia.R

--- Idesia.R
+++ Idesia.R
@@ -1,20 +1,11 @@
-library(googlesheets)
 library(dplyr)
 library(caret)
 library(ggplot2)
 library(alr3)
 #Lectura de datos#
-gs_auth()
-my_sheets <- gs_ls()
-gap <- gs_title("Experimentos")
-yul <- gap %>%
-  gs_read(ws = "Yuleysi")
-yul <- as.data.frame(yul)
-rm(my_sheets,gap)
+yul <- readRDS(file="D:/Documents/GitHub/LOI_research/loi.rds")
+yul$TEMP <- as.factor(yul$TEMP) ; yul$Repete <- as.factor(yul$Repete)
 
-yul$TEMP <- as.factor(yul$TEMP)
-yul$Repete <- as.factor(yul$Repete)
-yul <- subset(yul,MO_WyB < 8)
 
 
 ######################################
@@ -79,3 +70,43 @@ ggplot(yul,aes(x=MO_LOI,y=MO_WyB)) +
         axis.title.x = element_text(face="bold"),
         axis.title.y = element_text(face="bold"),
         legend.title=element_blank())
+
+
+
+
+
+##############################################
+#K-fold Cross validation for beta regression
+##############################################
+#data = data.frame con los datos
+#fm.es = Estructura del modelo
+#respuesta = indice de la variable respuesta en data
+#k y t = fold y repeticiones
+breg.cv <- function(data,fm.es,respuesta,k,t,...){
+  require("caret")
+  n <- nrow(data)
+  particion <- createMultiFolds(y=data[,respuesta],k = k,times = t)
+  modelos <- list()
+  MSE_te <- rep(0,times=length(particion))
+  pseR2 <- rep(0,times=length(particion))
+  phi <- rep(0,times=length(particion))
+  nk <- rep(0,times=length(particion))
+  for (j in 1:length(particion)) {
+    nk[j] <- n - length(particion[[j]])
+  }
+  for (i in 1:length(particion)) {
+    modelos[[i]] <- betareg(formula = fm.es,data = data[particion[[i]],])
+    MSE_te[i] <- mean((data[-particion[[i]],respuesta] - predict(modelos[[i]],data[-particion[[i]],]))^2)
+    pseR2[i] <- modelos[[i]]$pseudo.r.squared
+    phi[i] <- modelos[[i]]$coefficients$precision
+  }
+  MSE_te <- matrix(MSE_te,nrow = t,ncol = k)
+  pseR2 <- matrix(pseR2,nrow = t,ncol = k)
+  phi <- matrix(phi,nrow = t,ncol = k)
+  nk <- matrix(nk,nrow=t,ncol=k)
+  MSE_CV <- rowSums(MSE_te*nk)/n
+  pseR2_CV <- rowSums(pseR2*nk)/n
+  phi_CV <- rowSums(phi*nk)/n
+  return(list("MSE"=mean(MSE_CV),"MSE_sd"=sd(MSE_CV),"pR2"=mean(pseR2_CV),"pR2_sd"=sd(pseR2_CV),
+              "phi"=mean(phi_CV),"phi_sd"=sd(phi_CV)))
+}
